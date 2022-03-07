99de1175e0bc5708d33f6e676bea9d44e708d500
Roiney Beal <nei_beal@hotmail.com>
2022-03-07 18:50:00 -0300

    comparaçao entre 3 numeros

--- modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,5 +1,15 @@
 let a = 4;
 let b = 2;
+let c = 10;
+const d = 0;
+
+// triangulo
+
+const e = 10;
+const f = 10;
+const g = 10;
+
+// operaçãoes
 
 console.log(a+b);
 console.log(a-b);
@@ -7,8 +17,29 @@ console.log(a*b);
 console.log(a/b);
 console.log(a%b);
 
+// compração com 2 variaveis
+
 if (a > b){
     console.log(a);
 } else {
     console.log(b)
 }
+
+// compração com 3 variaveis
+
+if (a > b && a > c){
+    console.log(a)
+} else if ( b > a && b > c){
+    console.log (b)
+ } else {
+     console.log(c)
+ }
+
+ // 4
+ if (d > 0){
+     console.log("positive")
+ } else if (d < 0){
+     console.log("negative")
+ } else {
+     console.log("zero")
+ }
