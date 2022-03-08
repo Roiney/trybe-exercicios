6e88f959ff11bad773fd54d970b64b681cdf58f9
Roiney Beal <nei_beal@hotmail.com>
2022-03-08 18:17:28 -0300

    exercicios

---
+++ modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/scrips.js
@@ -0,0 +1,13 @@
+let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma =0;
+
+// Imprimir Array
+
+console.log(number);
+
+//SOMA
+
+for (i = 0; i < number.length; i++){
+   soma += number[i]
+  }
+  console.log(soma)
