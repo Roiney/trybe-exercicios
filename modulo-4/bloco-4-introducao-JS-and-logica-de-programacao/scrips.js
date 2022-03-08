00d8efed8810695afb425c3908af9adf5de72aab
Roiney Beal <nei_beal@hotmail.com>
2022-03-08 18:22:25 -0300

    media

--- modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/scrips.js
+++ modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/scrips.js
@@ -10,4 +10,14 @@ console.log(number);
 for (i = 0; i < number.length; i++){
    soma += number[i]
   }
-  console.log(soma)
+  console.log(soma);
+
+  // MÉdia aritimética
+
+  let media = soma/number.length;
+  console.log(media);
+  if (media < 20){
+    console.log("valor menor ou igual a 20")
+  } else {
+    console.log("valor maior que 20")
+  }
