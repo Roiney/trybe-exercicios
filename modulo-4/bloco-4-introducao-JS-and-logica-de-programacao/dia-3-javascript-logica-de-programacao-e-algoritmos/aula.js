b48c7ceccedd91561abc91733329e3da54b63333
Roiney Beal <nei_beal@hotmail.com>
2022-03-09 17:58:29 -0300

    aula

---
+++ modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/aula.js
@@ -0,0 +1,73 @@
+var resultado = 0;
+for (var i = 50 ; i <= 100; i++) {
+    resultado += i;
+}
+console.log("A soma de 50 a 100 é:", resultado);
+
+
+
+//divisão
+
+let divisiveis =[];
+
+for ( var i = 1; i <=125; i++){
+    if ( i % 3 === 0){
+        divisiveis.push(i)
+    }
+}
+
+console.log(divisiveis.length);
+
+if (divisiveis.length === 50){
+    console.log("é 50")
+}
+
+//Còdigo INTERNET https://pt.stackoverflow.com/questions/250079/pedra-papel-tesoura
+let jogador1 = "papel";
+let jogador2 = "tesoura"
+
+if (jogador1 == "pedra") {
+    if (jogador2 == "tesoura") {
+         ("Pedra quebra tesoura");
+    } else if (jogador2 == "papel"){
+       console.log("Papel envolve a Pedra");
+    } else if (jogador2 == "pedra") {
+       console.log("Empate");
+    }
+
+}
+else if (jogador1 == "tesoura") {
+    if (jogador2 == "pedra") {
+       console.log("Pedra quebra tesoura");
+    } else if (jogador2 == papel){
+       console.log("tesoura corta papel");
+    } else if (jogador2 == "tesoura") {
+       console.log("Empate");
+    }
+}
+
+else if (jogador1 == "papel"){
+    if (jogador2 == "pedra") {
+       console.log("Papel envolve pedra");
+    }else if (jogador2 == "tesoura") {
+       console.log("Tesoura corta papel");
+    }else if (jogador2 == "papel")  {
+       console.log("Empate");
+    }
+
+}
+
+let Marcerlla = 50;
+let Carlos = 30;
+let Camila = 40;
+let pessoaMaisNova = Marcerlla;
+
+if (Marcerlla < pessoaMaisNova){
+ Marcela = pessoaMaisNova
+} else if (Camila < pessoaMaisNova) {
+    Camila = pessoaMaisNova
+} else if (Carlos < pessoaMaisNova){
+    Carlos = pessoaMaisNova
+}
+
+console.log(pessoaMaisNova);
--- modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,13 +1,40 @@
-var fatorial = 10;
-var resultado = fatorial;
-for (var i = 1; i < fatorial; i++) {
-    resultado *= i;
+//fatotial
+
+var resultado = 
+for (var i = 50 ; i =< 100; i++) {
+    resultado += i;
 }
 console.log(resultado);
 
+
+//Palavra inversa
 var word ='tryber';
 var inverso = [];
 for (var i=0; i < word.length; i++){
   inverso.push(word[i])
 }
 console.log(inverso)
+
+// Maior Paravra
+let array = ['java', 'javascript', 'python', 'html', 'css']
+let maior = ['java']
+
+for (let i = 1; i < array.length; i++) {
+    if (maior.length < array[i].length) {
+      maior = array[i];
+    }
+  }
+  
+  console.log('O maior palavra é ', maior);
+
+  // Menor Palavra
+
+  for (let i = 1; i < array.length; i++) {
+    if (maior.length > array[i].length) {
+      maior = array[i];
+    }
+  }
+  
+  console.log('O menor palavra é ', maior);
+
+ 
