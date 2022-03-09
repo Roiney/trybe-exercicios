cb92ca67bee1ed57f00d282235c95fce0f68b66f
Roiney Beal <nei_beal@hotmail.com>
2022-03-09 15:48:32 -0300

    inicio exercicios

---
+++ modulo-4/bloco-4-introducao-JS-and-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,13 @@
+var fatorial = 10;
+var resultado = fatorial;
+for (var i = 1; i < fatorial; i++) {
+    resultado *= i;
+}
+console.log(resultado);
+
+var word ='tryber';
+var inverso = [];
+for (var i=0; i < word.length; i++){
+  inverso.push(word[i])
+}
+console.log(inverso)
