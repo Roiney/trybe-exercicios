ca679c68ee7074fc6bb43e98db0c2e2f61fa127b
Roiney Beal <nei_beal@hotmail.com>
2022-03-16 10:58:21 -0300

    parte 8

--- modulo-5/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ modulo-5/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,13 +1,51 @@
-var elemento_pai =document.body;
-var titulo = document.createElement('h1');
+let elemento_pai = document.body;
+let titulo = document.createElement("h1");
 elemento_pai.appendChild(titulo);
 
-var principal = document.createElement('main');
+let principal = document.createElement("main");
 elemento_pai.appendChild(principal);
-principal.className = 'main-content';
+principal.className = "main-content";
 
-var secao = document.createElement('section');
+let secao = document.createElement("section");
 principal.appendChild(secao);
 
-var paragrafo = document.createElement('p');
-secao.appendChild(paragrafo)
+let paragrafo = document.createElement("p");
+secao.appendChild(paragrafo);
+paragrafo.innerText = "texto";
+
+let leftSection = document.createElement("section");
+principal.appendChild(leftSection);
+leftSection.className = "left-content";
+
+let rightSection = document.createElement("section");
+principal.appendChild(rightSection);
+rightSection.className = "right-content";
+
+let imgLeftSection = document.createElement("img");
+imgLeftSection.className = "small-image";
+imgLeftSection.src = "https://picsum.photos/200";
+leftSection.appendChild(imgLeftSection);
+
+let addUlList = document.createElement("ul");
+rightSection.appendChild(addUlList);
+let ulList = [
+  "um",
+  "dois",
+  "três",
+  "quatro",
+  "cinco",
+  "seis",
+  "sete",
+  "oito",
+  "nove",
+  "dez",
+];
+let numeros = document.querySelector(".right-content");
+for(i=0; i < ulList.length; i++){
+     let numero = ulList[i];
+     let lista = document.createElement('li');
+     lista.innerText = numero;
+
+     addUlList.appendChild(lista)
+
+ };
